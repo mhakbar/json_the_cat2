44a6602491cab2578043534d6cb889a030f6e8a0
mhakbar <habibakbar@live.com>
2022-01-20 18:21:44 -0800

    function exported to index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,27 @@
 const request = require('request');
 const args = (process.argv.slice(2));
 
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, function(error, response, body) {
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, function (error, response, body) {
+    if (error) {
+      console.error("ERROR IS THIS: ", error);
+      
+      console.log(null);
 
-  if (error) {
-    console.error("ERROR IS THIS: ", error);
-  } else {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-    console.log("breed not found");
     } else {
-    //console.log(data[0]['description']);  
-    console.log(data);
-    console.log(typeof data);
-  }
-}
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        console.log("breed not found");
+      } else {
+        console.log(data);
+        console.log(typeof data);
+      }
+    }
 
-});
+  });
 
+};
+
+
+module.exports = { fetchBreedDescription };
